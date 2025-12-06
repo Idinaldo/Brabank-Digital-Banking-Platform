069bf167140c62748a019750da6b1ea518f3d182
Idinaldo <idinaldooliveira220@gmail.com>
2025-12-06 18:39:55 -0300

    feat: add User Request Data Transfer Object (DTO) with validations and regex patterns

---
+++ auth-service/src/main/java/dev/idinaldo/brabank/auth_service/model/user/UserRequestDTO.java
@@ -0,0 +1,45 @@
+package dev.idinaldo.brabank.auth_service.model.user;
+
+
+import dev.idinaldo.brabank.auth_service.model.role.Role;
+import jakarta.validation.constraints.*;
+import java.util.Set;
+
+public record UserRequestDTO(
+        @NotBlank(message = "E-mail é obrigatório")
+        @Email(message = "E-mail inválido")
+        String email,
+
+        @NotBlank
+        @Pattern(
+            regexp = "^[A-ZÀ-Úa-zà-ú]{2,}(?:[ '-][A-ZÀ-Úa-zà-ú]{2,})+$",
+            message = "Nome inválido"
+        )
+        String fullName,
+
+        @NotBlank(message = "Senha é obrigatório")
+        @Size(min = 8)
+        @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@.!#$%¨&*()_\\-+=`´~^\\\\[\\]{}:;?><\\/|])[A-Za-z\\d@.!#$%¨&*()_\\-+=`´~^\\\\[\\]{}:;?><\\/|]{8,}$",
+            message = "Senha inválida"
+        )
+        String password,
+
+        @NotBlank(message = "Nome de usuário é obrigatório")
+        @Size(min = 2, max = 15)
+        @Pattern(
+            regexp = "^[0-9A-Za-z_\\.:\\/]{2,}$",
+            message = "Nome de usuário inválido"
+        )
+        String username,
+
+        @NotBlank(message = "CPF é obrigatório")
+        @Pattern(
+            regexp = "^\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}$",
+            message = "CPF inválido"
+        )
+        String cpf,
+
+        @NotEmpty Set<Role> roles
+) {
+}
