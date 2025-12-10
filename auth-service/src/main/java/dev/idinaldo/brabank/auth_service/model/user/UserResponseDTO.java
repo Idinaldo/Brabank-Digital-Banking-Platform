2f3294e3124faa2a0fc1b420c3b00c37a2940983
Idinaldo <idinaldooliveira220@gmail.com>
2025-12-10 09:06:02 -0300

    refactor: update UserResponseDTO to be auth-based

--- auth-service/src/main/java/dev/idinaldo/brabank/auth_service/model/user/UserResponseDTO.java
+++ auth-service/src/main/java/dev/idinaldo/brabank/auth_service/model/user/UserResponseDTO.java
@@ -3,7 +3,6 @@ package dev.idinaldo.brabank.auth_service.model.user;
 import dev.idinaldo.brabank.auth_service.model.role.Role;
 import jakarta.validation.constraints.*;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.UUID;
@@ -14,19 +13,6 @@ public record UserResponseDTO(
         @NotBlank(message = "E-mail é obrigatório")
         @Email(message = "E-mail inválido")
         String email,
-
-        @NotBlank(message = "Nome completo é obrigatório")
-        @Pattern(
-            regexp = "^[A-ZÀ-Úa-zà-ú]{2,}(?:[ '-][A-ZÀ-Úa-zà-ú]{2,})+$",
-            message = "Nome inválido"
-        )
-        String fullName,
-
-        @NotBlank(message = "Senha é obrigatório")
-        @Pattern(
-            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@.!#$%¨&*()_\\-+=`´~^\\\\[\\]{}:;?><\\/|])[A-Za-z\\d@.!#$%¨&*()_\\-+=`´~^\\\\[\\]{}:;?><\\/|]{8,}$",
-            message = "Senha inválida"
-        )
 
         @Size(min = 8)
         @NotBlank(message = "Senha é obrigatório")
@@ -44,18 +30,8 @@ public record UserResponseDTO(
         )
         String username,
 
-        @NotBlank(message = "CPF é obrigatório")
-        @Pattern(
-            regexp = "^\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}$",
-            message = "CPF inválido"
-        )
-        String cpf,
-
         @NotEmpty(message = "Role é obrigatório")
         Set<Role> roles,
-
-        @NotNull(message = "Data de nascimento é obrigatório")
-        LocalDate birthDate,
 
         @NotNull(message = "createdAt é obrigatório")
         LocalDateTime createdAt,
