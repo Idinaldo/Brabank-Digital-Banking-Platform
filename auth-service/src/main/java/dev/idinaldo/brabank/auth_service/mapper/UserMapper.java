00ac0dc4b67f15a90d30dfac01af71ed5a786999
Idinaldo <idinaldooliveira220@gmail.com>
2025-12-13 20:24:21 -0300

    feat: update UserMapper to prevent sensitive data exposure

--- auth-service/src/main/java/dev/idinaldo/brabank/auth_service/mapper/UserMapper.java
+++ auth-service/src/main/java/dev/idinaldo/brabank/auth_service/mapper/UserMapper.java
@@ -1,8 +1,8 @@
 package dev.idinaldo.brabank.auth_service.mapper;
 
 import dev.idinaldo.brabank.auth_service.model.user.User;
-import dev.idinaldo.brabank.auth_service.model.user.UserRequestDTO;
-import dev.idinaldo.brabank.auth_service.model.user.UserResponseDTO;
+import dev.idinaldo.brabank.auth_service.dto.user.UserRequestDTO;
+import dev.idinaldo.brabank.auth_service.dto.user.UserResponseDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
