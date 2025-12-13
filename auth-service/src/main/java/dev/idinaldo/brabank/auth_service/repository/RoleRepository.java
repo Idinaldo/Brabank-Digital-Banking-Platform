d9d3df3ecbbffadc3c0dd20066783fed8f04a223
Idinaldo <idinaldooliveira220@gmail.com>
2025-12-13 20:33:05 -0300

    feat: add RoleRepository to handle persistence role operations

--- auth-service/src/main/java/dev/idinaldo/brabank/auth_service/repository/RoleRepository.java
+++ auth-service/src/main/java/dev/idinaldo/brabank/auth_service/repository/RoleRepository.java
@@ -10,5 +10,5 @@ import java.util.UUID;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, UUID> {
-    Optional<Role> findByName(RoleName roleName);
+    Optional<Role> findByName(RoleName name);
 }
