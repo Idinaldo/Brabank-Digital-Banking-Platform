7483519543ad4bac411e5d7aa2a7c73c05fcf7f4
Idinaldo <idinaldooliveira220@gmail.com>
2025-12-13 20:15:52 -0300

    feat: add role register endpoint

--- auth-service/src/main/java/dev/idinaldo/brabank/auth_service/controller/RoleController.java
+++ auth-service/src/main/java/dev/idinaldo/brabank/auth_service/controller/RoleController.java
@@ -1,6 +1,12 @@
 package dev.idinaldo.brabank.auth_service.controller;
 
+import dev.idinaldo.brabank.auth_service.dto.role.RoleRequestDTO;
+import dev.idinaldo.brabank.auth_service.dto.role.RoleResponseDTO;
+import dev.idinaldo.brabank.auth_service.service.RoleService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/roles")
 public class RoleController {
 
+    private final RoleService roleService;
+
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
     @PostMapping("/")
-    public String addRole() {
-        return "POST /api/roles/";
+    public ResponseEntity<RoleResponseDTO> addRole(@RequestBody @Valid RoleRequestDTO roleRequestDTO) {
+        return ResponseEntity.ok(roleService.addRole(roleRequestDTO));
     }
 }
