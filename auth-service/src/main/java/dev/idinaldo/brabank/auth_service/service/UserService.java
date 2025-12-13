273b32a52db38ea5e1ae5ddb6de2cc7afcc966f3
Idinaldo <idinaldooliveira220@gmail.com>
2025-12-13 20:35:34 -0300

    feat: update UserService to implement default role (ROLE_CLIENT) in register.
    This was a necessary change because as it was possible to send role in the request anyone could be admin and manage important info and security details

--- auth-service/src/main/java/dev/idinaldo/brabank/auth_service/service/UserService.java
+++ auth-service/src/main/java/dev/idinaldo/brabank/auth_service/service/UserService.java
@@ -1,13 +1,15 @@
 package dev.idinaldo.brabank.auth_service.service;
 
+import dev.idinaldo.brabank.auth_service.dto.user.UserRequestDTO;
+import dev.idinaldo.brabank.auth_service.dto.user.UserResponseDTO;
 import dev.idinaldo.brabank.auth_service.mapper.UserMapper;
 import dev.idinaldo.brabank.auth_service.model.role.Role;
 import dev.idinaldo.brabank.auth_service.model.role.RoleName;
 import dev.idinaldo.brabank.auth_service.model.user.User;
-import dev.idinaldo.brabank.auth_service.model.user.UserRequestDTO;
-import dev.idinaldo.brabank.auth_service.model.user.UserResponseDTO;
 import dev.idinaldo.brabank.auth_service.repository.RoleRepository;
 import dev.idinaldo.brabank.auth_service.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -20,6 +22,7 @@ public class UserService {
     private final UserMapper userMapper;
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     public UserService(UserMapper userMapper, UserRepository userRepository, RoleRepository roleRepository) {
         this.userMapper = userMapper;
@@ -27,24 +30,18 @@ public class UserService {
         this.roleRepository = roleRepository;
     }
 
+    // TODO: add a more complete exception handler
     public UserResponseDTO register(UserRequestDTO userRequestDTO) {
         try {
-            Set<Role> roles = Set.of();
-
-            for (RoleName role : userRequestDTO.roles()) {
-                roles.add(roleRepository.findByName(role).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "INVALID ROLE")));
-            }
-
             User user = userMapper.userRequestDtoToUser(userRequestDTO);
-            user.setRoles(roles);
-            System.out.println("USER: " + user);
+            Role client_role = roleRepository.findByName(RoleName.CLIENT).orElseThrow(() -> new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "ROLE_CLIENT NOT FOUND"));
+            user.setRoles(Set.of(client_role));
             User saved_user = userRepository.save(user);
             return userMapper.userToUserResponseDto(saved_user);
         } catch (Exception e) {
             // temporary exception "handler"
-            // TODO: add a more complete exception handler
-            System.out.println("ERROR: " + e);
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "BAD REQUEST");
+            logger.info("ERROR: " + e);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.toString());
         }
     }
 }
