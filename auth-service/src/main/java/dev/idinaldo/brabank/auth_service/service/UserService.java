d7f8ae58274efa0fd936511b535bbd2edab19e6f
Idinaldo <idinaldooliveira220@gmail.com>
2025-12-14 03:38:43 -0300

    feat: add PasswordService and update RoleService use

--- auth-service/src/main/java/dev/idinaldo/brabank/auth_service/service/UserService.java
+++ auth-service/src/main/java/dev/idinaldo/brabank/auth_service/service/UserService.java
@@ -2,15 +2,19 @@ package dev.idinaldo.brabank.auth_service.service;
 
 import dev.idinaldo.brabank.auth_service.dto.user.UserRequestDTO;
 import dev.idinaldo.brabank.auth_service.dto.user.UserResponseDTO;
+import dev.idinaldo.brabank.auth_service.infra.security.SecurityBeansConfig;
 import dev.idinaldo.brabank.auth_service.mapper.UserMapper;
 import dev.idinaldo.brabank.auth_service.model.role.Role;
 import dev.idinaldo.brabank.auth_service.model.role.RoleName;
 import dev.idinaldo.brabank.auth_service.model.user.User;
 import dev.idinaldo.brabank.auth_service.repository.RoleRepository;
 import dev.idinaldo.brabank.auth_service.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -21,27 +25,35 @@ public class UserService {
 
     private final UserMapper userMapper;
     private final UserRepository userRepository;
-    private final RoleRepository roleRepository;
+    private final RoleService roleService;
     private final Logger logger = LoggerFactory.getLogger(UserService.class);
+    private final PasswordService passwordService;
 
-    public UserService(UserMapper userMapper, UserRepository userRepository, RoleRepository roleRepository) {
+    public UserService(UserMapper userMapper, UserRepository userRepository, RoleService roleService, PasswordService passwordService) {
         this.userMapper = userMapper;
         this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
+        this.roleService = roleService;
+        this.passwordService = passwordService;
     }
 
     // TODO: add a more complete exception handler
-    public UserResponseDTO register(UserRequestDTO userRequestDTO) {
+    @Transactional
+    public UserResponseDTO registerClient(UserRequestDTO userRequestDTO) {
         try {
             User user = userMapper.userRequestDtoToUser(userRequestDTO);
-            Role client_role = roleRepository.findByName(RoleName.CLIENT).orElseThrow(() -> new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "ROLE_CLIENT NOT FOUND"));
-            user.setRoles(Set.of(client_role));
-            User saved_user = userRepository.save(user);
-            return userMapper.userToUserResponseDto(saved_user);
+
+            Role clientRole = roleService.findRoleByName(RoleName.CLIENT);
+            user.setRoles(Set.of(clientRole));
+
+            user.setPasswordHash(passwordService.hash(userRequestDTO.password()));
+            User persistedUser = userRepository.save(user);
+
+            return userMapper.userToUserResponseDto(persistedUser);
         } catch (Exception e) {
             // temporary exception "handler"
             logger.info("ERROR: " + e);
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.toString());
         }
     }
+
 }
