def2ee5e03fbd449523435325a49e6dcffb99fea
Idinaldo <idinaldooliveira220@gmail.com>
2025-12-14 03:39:05 -0300

    feat: add security dedicated directory

---
+++ auth-service/src/main/java/dev/idinaldo/brabank/auth_service/infra/security/SecurityBeansConfig.java
@@ -0,0 +1,15 @@
+package dev.idinaldo.brabank.auth_service.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityBeansConfig {
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
