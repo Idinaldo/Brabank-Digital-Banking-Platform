5b987a54240395f1b92f6a9288b615c6aa7c011e
Idinaldo <idinaldooliveira220@gmail.com>
2025-12-17 07:48:08 -0300

    feat: add mapping configurations to userRequestDtoToUser method

--- auth-service/src/main/java/dev/idinaldo/brabank/auth_service/mapper/RoleMapper.java
+++ auth-service/src/main/java/dev/idinaldo/brabank/auth_service/mapper/RoleMapper.java
@@ -4,9 +4,13 @@ import dev.idinaldo.brabank.auth_service.dto.role.RoleResponseDTO;
 import dev.idinaldo.brabank.auth_service.model.role.Role;
 import dev.idinaldo.brabank.auth_service.model.role.RoleName;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface RoleMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
     Role roleRequestDtoToRole(RoleName name);
     RoleResponseDTO roleToRoleResponseDto(Role role);
 }
