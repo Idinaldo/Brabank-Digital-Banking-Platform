3930bbdf54b1c6db764d79693858ce08e62dfcb7
Idinaldo <idinaldooliveira220@gmail.com>
2025-12-20 11:40:29 -0300

    refactor(userMapper/mapStruct): ignore userSubject, createdAt, updatedAt, id and passwordHash in userRequestDtoToUser method

--- auth-service/src/main/java/dev/idinaldo/brabank/auth_service/mapper/UserMapper.java
+++ auth-service/src/main/java/dev/idinaldo/brabank/auth_service/mapper/UserMapper.java
@@ -1,16 +1,28 @@
 package dev.idinaldo.brabank.auth_service.mapper;
 
+import dev.idinaldo.brabank.auth_service.model.role.Role;
+import dev.idinaldo.brabank.auth_service.model.role.RoleName;
 import dev.idinaldo.brabank.auth_service.model.user.User;
 import dev.idinaldo.brabank.auth_service.dto.user.UserRequestDTO;
 import dev.idinaldo.brabank.auth_service.dto.user.UserResponseDTO;
 import lombok.Builder;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-    @Mapping(source = "password", target = "passwordHash")
+    @Mapping(target = "id", ignore = true)
     @Mapping(target = "roles", ignore = true)
+    @Mapping(target = "userSubject", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "passwordHash", ignore = true)
     User userRequestDtoToUser(UserRequestDTO userRequestDTO);
     UserResponseDTO userToUserResponseDto(User user);
+
 }
